33933ca1e350b69167e774380178661676dbd723
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2025-08-20 23:15:27 -0700

    fixed -> LoginController.java, it was missing @Controller

--- src/main/java/com/budgetingapplication/controller/LoginController.java
+++ src/main/java/com/budgetingapplication/controller/LoginController.java
@@ -1,9 +1,9 @@
 package com.budgetingapplication.controller;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Component
+@Controller
 public class LoginController {
 
     @GetMapping("/login")
